f0d2c45439d4517d963d66c32e10c90d24563e29
QadomyHomyt <suleimana@homyt.com>
2021-02-23 00:57:40 +0200

    create instructions screen and display instructions food

--- app/src/main/java/com/qadomy/foody/ui/screens/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/qadomy/foody/ui/screens/instructions/InstructionsFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.qadomy.foody.R
+import com.qadomy.foody.model.Result
+import com.qadomy.foody.utils.Constants
+import com.qadomy.foody.utils.Constants.Companion.PARCELABLE_KEY
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
 
 class InstructionsFragment : Fragment() {
 
@@ -14,7 +19,17 @@ class InstructionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val view  = inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        // read data from details activity
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(PARCELABLE_KEY)
+
+        view.instructions_webView.webViewClient = object : WebViewClient() {}
+        val websiteUrl: String = myBundle!!.sourceUrl
+        view.instructions_webView.loadUrl(websiteUrl)
+
+        return  view
     }
 
 }
